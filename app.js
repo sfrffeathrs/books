e4d1285515c1a14a10fbc746fd01be9065e62c29
Sofia <sofia.volkova@voco.ee>
2023-01-05 12:48:13 +0200

    add book to book table

---
+++ app.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('#book-form')
+form.addEventListener('submit', addBook)
+
+function addBook(event){
+    //read user inputs
+    const title = document.querySelector('#title').value
+    const author = document.querySelector('#author').value
+    const isbn = document.querySelector('#isbn').value
+
+    //create table row with user input data
+    const bookRow = document.createElement('tr')
+    bookRow.innerHTML = `
+                        <td>${title}</td>
+                        <td>${author}</td>
+                        <td>${isbn}</td>
+                        <td><a href="#">X</a></td>`
+    const booksTable = document.querySelector('#books')
+    booksTable.appendChild(bookRow)
+
+    //add book to localStorage
+    const book = [title, author, isbn]
+    let books // array for user input
+    if(localStorage.getItem('books') === null){
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem('books'))
+    }
+    books.push(book)
+    localStorage.setItem('books', JSON.stringify(books))
+
+    //clear input
+    document.querySelector('#title').value = ''
+    document.querySelector('#author').value = ''
+    document.querySelector('#isbn').value = ''
+    event.preventDefault()
+}

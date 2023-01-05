22b14bccb919e73aac73821c1498e4c2e668a66a
Sofia <sofia.volkova@voco.ee>
2023-01-05 14:00:10 +0200

    delete single book from Local Storage

--- app.js
+++ app.js
@@ -3,6 +3,38 @@ form.addEventListener('submit', addBook)
 
 document.addEventListener('DOMContentLoaded', getBooks)
 
+const booksTable = document.querySelector('#books')
+booksTable.addEventListener('click', deleteBook)
+
+function deleteBook(event){
+    if(event.target.textContent === 'X'){
+        if(confirm('Are you sure you want to delete this book?')){
+            event.target.parentElement.parentElement.remove()
+            const isbn = event.target.parentElement.previousElementSibling
+            const author = event.target.parentElement.previousElementSibling.previousElementSibling
+            const title = event.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling
+
+            const book = [title.textContent, author.textContent, isbn.textContent]
+
+            let books // array for LS data
+            if(localStorage.getItem('books') === null){
+                books = []
+            } else {
+                books = JSON.parse(localStorage.getItem('books'))
+            }
+
+            books.forEach(function(bookFromLS, index){
+                if(bookFromLS[0] === book[0] &&
+                    bookFromLS[1] === book[1] &&
+                    bookFromLS[2] === book[2]){
+                    books.splice(index, 1)
+                }
+            })
+            localStorage.setItem('books', JSON.stringify(books))
+        }
+    }
+}
+
 function getBooks(){
     let books // array for LS data
     if(localStorage.getItem('books') === null){

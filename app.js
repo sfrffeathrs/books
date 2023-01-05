4247f882a848a217bd8dc98c88db3276834880db
Sofia <sofia.volkova@voco.ee>
2023-01-05 13:00:06 +0200

    get books from localStorage

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const form = document.querySelector('#book-form')
 form.addEventListener('submit', addBook)
 
+document.addEventListener('DOMContentLoaded', getBooks)
+
+function getBooks(){
+    let books // array for LS data
+    if(localStorage.getItem('books') === null){
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem('books'))
+    }
+    books.forEach(function(book){
+        const bookRow = document.createElement('tr')
+        bookRow.innerHTML = `
+                        <td>${book[0]}</td>
+                        <td>${book[1]}</td>
+                        <td>${book[2]}</td>
+                        <td><a href="#">X</a></td>`
+        const booksTable = document.querySelector('#books')
+        booksTable.appendChild(bookRow)
+    })
+}
+
 function addBook(event){
     //read user inputs
     const title = document.querySelector('#title').value
